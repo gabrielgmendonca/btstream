dbf43228d41aea39a33de443aed79805ddb4555c
Gabriel Mendonça <gabrielgmendonca@poli.ufrj.br>
2012-03-11 19:39:35 -0300

    Add example PiecePicker: SequentialPiecePicker.

---
+++ libbtstream/include/SequentialPiecePicker.h
@@ -0,0 +1,46 @@
+/*
+ * Copyright (C) 2011-2012 Gabriel Mendonça
+ *
+ * This file is part of BTStream.
+ * BTStream is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * BTStream is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with BTStream.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *
+ * SequentialPiecePicker.h
+ *
+ *  Created on: 11/03/2012
+ *      Author: gabriel
+ */
+
+#ifndef SEQUENTIALPIECEPICKER_H_
+#define SEQUENTIALPIECEPICKER_H_
+
+#include "PiecePicker.h"
+
+namespace btstream {
+
+class SequentialPiecePicker: public PiecePicker {
+public:
+	SequentialPiecePicker();
+
+	/**
+	 * Returns each piece index sequentially.
+	 */
+	virtual int pick_piece(torrent* t);
+
+private:
+	int m_counter;
+};
+
+} /* namespace btstream */
+#endif /* SEQUENTIALPIECEPICKER_H_ */
