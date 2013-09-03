5efc6b32021dd2e7f9eaa1103a2442e8cbb15d28
Gabriel Mendonça <gabrielgmendonca@poli.ufrj.br>
2013-09-03 17:26:25 -0300

    Shrink torrent test files to speed up tests.

--- libbtstream/test/constants.cpp
+++ libbtstream/test/constants.cpp
@@ -10,11 +10,11 @@
 namespace btstream {
 
 std::string TEST_TORRENT1 = "testfile1.torrent";
-int TEST_TORRENT1_PIECES = 256;
+int TEST_TORRENT1_PIECES = 8;
 int TEST_TORRENT1_PIECE_LENGTH = 1024;
 
 std::string TEST_TORRENT2 = "testfile2.torrent";
-int TEST_TORRENT2_PIECES = 1024;
+int TEST_TORRENT2_PIECES = 16;
 int TEST_TORRENT2_PIECE_LENGTH = 512;
 
 }
